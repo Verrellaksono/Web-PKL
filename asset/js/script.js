9c260742aacb09b1e8584832718db206f106e5fd
Paradiess-svg <110276580+Paradiess-svg@users.noreply.github.com>
2023-01-13 10:03:32 +0700

    Gua gabungin cuy

--- asset/js/script.js
+++ asset/js/script.js
@@ -25,3 +25,28 @@ $(document).ready(function () {
         }
     });
 });
+
+//FOOTER JS
+
+let verrel = document.getElementById('rel').innerHTML;
+const copyContentrel = async () => {
+    try {
+        await navigator.clipboard.writeText(verrel);
+        alert("Verrel's E-mail copied")
+        // console.log(document.getElementById("demo").innerHTML = "E-mail coppied!");
+        // $("#demo").fadeOut ("slow");
+        //Kaga bisa njirrr
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
+
+let dihar = document.getElementById('iz').innerHTML;
+const copyContentiz = async () => {
+    try {
+        await navigator.clipboard.writeText(dihar);
+        alert("Izdihar's E-mail copied")
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
